1de670476724b388010b282bcf3f12351531c208
DmanCoder <ktdenis.1@live.com>
2023-05-10 19:27:19 +1000

    TEST

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import Image from "next/image";
+"use client";
 
 export default function Home() {
   return (
@@ -34,6 +34,7 @@ export default function Home() {
             required
           />
         </div>
+
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
